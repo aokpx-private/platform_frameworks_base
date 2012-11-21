0f5f5c037e50fc3e36b3dc1885b5bbad6ff6cc91
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-21 01:42:32 -0500

    Quick Settings: Make Wifi and Bluetooth Turn on and off
    short press turns on and off
    long press goes to settings.service
    
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -95,6 +95,7 @@ class QuickSettings {
 
     private DisplayManager mDisplayManager;
     private WifiDisplayStatus mWifiDisplayStatus;
+    private WifiManager wifiManager;
     private PhoneStatusBar mStatusBarService;
     private BluetoothState mBluetoothState;
     private BluetoothAdapter mBluetoothAdapter;
@@ -135,6 +136,7 @@ class QuickSettings {
         mContainerView = container;
         mModel = new QuickSettingsModel(context);
         mWifiDisplayStatus = new WifiDisplayStatus();
+        wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mBluetoothState = new QuickSettingsModel.BluetoothState();
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
@@ -392,7 +394,6 @@ class QuickSettings {
                 intent.setComponent(ComponentName.unflattenFromString("com.aokp.romcontrol/.ROMControlActivity"));
                 intent.addCategory("android.intent.category.LAUNCHER");
                 startSettingsActivity(intent);
-                getService().animateCollapsePanels();
                 return true;
             }
         });
@@ -415,7 +416,19 @@ class QuickSettings {
         wifiTile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (wifiManager.isWifiEnabled()) {
+                    wifiManager.setWifiEnabled(false);
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                }
+                getService().animateCollapsePanels();
+            }
+        });
+        wifiTile.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
                 startSettingsActivity(android.provider.Settings.ACTION_WIFI_SETTINGS);
+                return true;
             }
         });
         if (LONG_PRESS_TOGGLES) {
@@ -586,7 +599,20 @@ class QuickSettings {
             bluetoothTile.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+                    if (adapter.isEnabled()) {
+                        adapter.disable();
+                    } else {
+                        adapter.enable();
+                    }
+                getService().animateCollapsePanels();
+                }
+            });
+            bluetoothTile.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
                     startSettingsActivity(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                    return true;
                 }
             });
             if (LONG_PRESS_TOGGLES) {
