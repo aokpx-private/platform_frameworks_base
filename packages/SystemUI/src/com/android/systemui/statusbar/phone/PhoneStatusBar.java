22983288396a6a731e76bcba2f2627eaa0df3bcc
Roman Birg <romanbirg@gmail.com>
2012-12-29 19:58:01 -0600

    transparent statusbar
    Currently just uses the same logic/values as the nav bar. We may want to separate those out
    
      Patch Set 9: Fix whitespace before merge
    
    Change-Id: I6188ebd98684c8775b0447b4e6a1f09ec1a78887
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -651,7 +651,6 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         // listen for USER_SETUP_COMPLETE setting (per-user)
         resetUserSetupObserver();
-
         return mStatusBarView;
     }
 
@@ -1321,6 +1320,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                 haltTicker();
             }
         }
+        mStatusBarView.updateBackgroundAlpha();
     }
 
     @Override
@@ -1930,6 +1930,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     public void topAppWindowChanged(boolean showMenu) {
+        mStatusBarView.updateBackgroundAlpha();
         if (DEBUG) {
             Slog.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
         }
